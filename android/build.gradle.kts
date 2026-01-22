3c7b9e2cfb8e0ce246576cfb32178815dc225896
helangeorge999 <helangeorge78@gmail.com>
2026-01-22 13:09:30 +0545

    code changed

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,18 @@
+// Top-level build.gradle.kts for Flutter + Kotlin 1.9.0
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Kotlin plugin compatible with Flutter stable
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        // Android Gradle Plugin compatible with Gradle 8.3.x
+        classpath("com.android.tools.build:gradle:8.3.1")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -5,6 +20,7 @@ allprojects {
     }
 }
 
+// Custom build directory setup
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
@@ -15,10 +31,13 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+// Make sure all subprojects are evaluated after app module
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Clean task
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
